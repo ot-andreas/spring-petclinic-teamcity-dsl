f4728ef53141459b7da4581308bc353558bbdf8f
Andreas Lidholm <elidholm@opentable.com>
2021-02-17 09:32:19 -0800

    And add back a script step

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -52,6 +52,13 @@ object Build : BuildType({
             dockerImage = "maven:3.6.0-jdk-8"
         }
 
+        script {
+            name = "tester"
+            scriptContent = "echo 1"
+            param("org.jfrog.artifactory.selectedDeployableServer.downloadSpecSource", "Job configuration")
+            param("org.jfrog.artifactory.selectedDeployableServer.useSpecs", "false")
+            param("org.jfrog.artifactory.selectedDeployableServer.uploadSpecSource", "Job configuration")
+        }
     }
     triggers {
         vcs {
